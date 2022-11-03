aeb1827a5fb2f09c00bb3f247e714561a25c22ef
kral;git config --global user.name kral <petri.2003@outlook.com>
2022-11-03 10:07:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int MultiArr[4][4] = {
+        {1, 1, 1, 1},
+        {2, 2, 2, 2},
+        {3, 3, 3, 3},
+        {4, 4, 4, 4}
+    };
+    for (int i = 0; i < 4; i++) {
+        for (int a = 0; a < 4; a++) {
+            cout << MultiArr[i][a];
+        }
+        cout << endl;
+    };
+
+    cout << endl;
+
+    int Arr[4];
+        for (int i = 0; i < 4; i++) {
+            Arr[i] = MultiArr[0][i];
+            MultiArr[0][i] = MultiArr[2][i];
+            MultiArr[2][i] = Arr[i];
+        }
+     for (int i = 0; i < 4; i++) {
+         for (int a = 0; a < 4; a++) {
+             cout << MultiArr[i][a];
+         }
+         cout << endl;
+     };
+    
+
+    return 0;
+}
